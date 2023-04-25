71fff49adb7d1b1cf46cdb972f95914e7e4b0fdb
vbetsch <vic.betsch@gmail.com>
2023-04-25 16:48:08 +0200

    :sparkles: Enable logs

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, ttk, StringVar, PhotoImage
+from tkinter import Tk, ttk, StringVar, PhotoImage, BooleanVar
 
 
 class Test:
@@ -13,28 +13,37 @@ class Test:
         self.frame.pack()
 
         self.count = 0
-        self.text = StringVar()
-        self.text.set(str(self.count))
+        self.text = StringVar(value=str(self.count))
         self.image = PhotoImage(file='assets/pycharm.png')
+        self.enable_logs_default = BooleanVar(value=True)
 
         self.picture = ttk.Label(image=self.image)
         self.label = ttk.Label(self.frame, textvariable=self.text, font="Poppins")
         self.button = ttk.Button(self.frame, text="+1", default="active", command=self.test)
+        self.check = ttk.Checkbutton(self.frame, text='Enable logs',
+                                     command=self.toggle_logs, variable=self.enable_logs_default)
 
         window.bind('<Return>', lambda e: self.button.invoke())
 
         # self.picture.pack()
         self.label.pack()
         self.button.pack()
+        self.check.pack()
 
     def test(self):
         if self.count >= 10:
-            print("ERROR: You have reached the limit")
+            print("You have reached the limit")
             self.button.state(['disabled'])
             pass
         else:
-            print("+1")
+            if self.check.instate(['selected']):
+                print("+1")
             self.count += 1
             self.text.set(str(self.count))
             # print(f"Count: {self.count}")
 
+    def toggle_logs(self):
+        if self.check.instate(['selected']):
+            print("Enable logs")
+        else:
+            print("Disable logs")
