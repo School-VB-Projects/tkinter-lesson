8beb34dcc8b8ddea6952dcfc7b9e7cd526362a80
vbetsch <vic.betsch@gmail.com>
2023-04-25 11:22:02 +0200

    :sparkles: Print Hierarchy

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from tkinter import Tk
 from examples.FeetToMeters import FeetToMeters
+from cli import print_hierarchy
 
 
 def run() -> None:
     window = Tk()
     FeetToMeters(window)
+    print_hierarchy(window)
     window.mainloop()
 
 
