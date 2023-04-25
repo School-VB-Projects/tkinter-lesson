32cf5c2949a7e6b2d41656c37c4acba73e3005f8
vbetsch <vic.betsch@gmail.com>
2023-04-25 16:52:29 +0200

    :label: Fix count type

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, ttk, StringVar, PhotoImage, BooleanVar
+from tkinter import Tk, ttk, PhotoImage, BooleanVar, IntVar
 
 
 class Test:
@@ -12,13 +12,12 @@ class Test:
 
         self.frame.pack()
 
-        self.count = 0
-        self.text = StringVar(value=str(self.count))
+        self.count = IntVar(value=0)
         self.image = PhotoImage(file='assets/pycharm.png')
         self.enable_logs_default = BooleanVar(value=True)
 
         self.picture = ttk.Label(image=self.image)
-        self.label = ttk.Label(self.frame, textvariable=self.text, font="Poppins")
+        self.label = ttk.Label(self.frame, textvariable=self.count, font="Poppins")
         self.button = ttk.Button(self.frame, text="+1", default="active", command=self.test)
         self.check = ttk.Checkbutton(self.frame, text='Enable logs',
                                      command=self.toggle_logs, variable=self.enable_logs_default)
@@ -31,15 +30,15 @@ class Test:
         self.check.pack()
 
     def test(self):
-        if self.count >= 10:
+        count = self.count.get()
+        if count >= 10:
             print("You have reached the limit")
             self.button.state(['disabled'])
             pass
         else:
             if self.check.instate(['selected']):
                 print("+1")
-            self.count += 1
-            self.text.set(str(self.count))
+            self.count.set(count + 1)
             # print(f"Count: {self.count}")
 
     def toggle_logs(self):
