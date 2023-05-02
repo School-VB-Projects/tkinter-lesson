1fc1860b4ccb3c20bdfa82e3d475476796f35f98
vbetsch <vic.betsch@gmail.com>
2023-05-02 10:01:33 +0200

    :art: Clean code

--- src/Counter.py
+++ src/Counter.py
@@ -1,8 +1,8 @@
 from enum import Enum
-from tkinter import Tk, ttk, BooleanVar, IntVar, Frame, Label
+from tkinter import Tk, ttk, BooleanVar, IntVar, Frame
 
 
-class States(Enum):
+class State(Enum):
     """States widgets"""
     SELECTED = 'selected'
     DISABLED = 'disabled'
@@ -10,15 +10,15 @@ class States(Enum):
     NOT_DISABLED = '!disabled'
 
 
-class Defaults(Enum):
+class Default(Enum):
     """Defaults values"""
-    FRAME = None
+    FRAME: Frame = None
     COUNT: int = 0
-    LOGS: bool = True
+    LOGS = True
     LIMIT: int = 5
-    RESET: bool = False
-    CHECK_STATE = [States.SELECTED.value]
-    BUTTON_STATE = [States.NOT_DISABLED.value]
+    RESET = False
+    CHECK_STATE: State = [State.SELECTED.value]
+    BUTTON_STATE: State = [State.NOT_DISABLED.value]
 
 
 class Counter:
@@ -26,25 +26,25 @@ class Counter:
     def __init__(self, window: Tk) -> None:
         # https://tkdocs.com/tutorial/concepts.html
         window.title('Counter')
-        self.frame = Defaults.FRAME.value
+        self.frame: Frame = Default.FRAME.value
         self.compute_frame(window)
 
-        self.enable_logs_default: BooleanVar = BooleanVar(value=bool(Defaults.LOGS.value))
-        self.count: IntVar = IntVar(value=Defaults.COUNT.value)
-        self.limit = IntVar(value=Defaults.LIMIT.value)
+        self.enable_logs_default: BooleanVar = BooleanVar(value=bool(Default.LOGS.value))
+        self.count: IntVar = IntVar(value=Default.COUNT.value)
+        self.limit: IntVar = IntVar(value=Default.LIMIT.value)
 
         # https://tkdocs.com/tutorial/widgets.html
-        self.count_label: Label = ttk.Label(
+        self.count_label = ttk.Label(
             self.frame,
             textvariable=self.count,
             font="Poppins"
         )
-        self.choose_entry_label: Label = ttk.Label(
+        self.choose_entry_label = ttk.Label(
             self.frame,
             text="Choose your entry",
             font="Poppins"
         )
-        self.custom_entry_label: Label = ttk.Label(
+        self.custom_entry_label = ttk.Label(
             self.frame,
             text="Set a custom entry",
             font="Poppins"
@@ -59,7 +59,7 @@ class Counter:
             self.frame,
             text="Reset",
             command=self.reset,
-            state=States.DISABLED.value
+            state=State.DISABLED.value
         )
         self.confirm = ttk.Button(
             self.frame,
@@ -103,25 +103,25 @@ class Counter:
 
         print("History")
 
-    def compute_frame(self, window: Tk):
+    def compute_frame(self, window: Tk) -> None:
         """Configure and compute the frame"""
         ttk.Style().configure('Danger.TFrame', borderwidth=5, relief='sunken')
-        self.frame: Frame = ttk.Frame(window, padding="20 20 20 20", style='Danger.TFrame')
+        self.frame = ttk.Frame(window, padding="20 20 20 20", style='Danger.TFrame')
         self.frame.pack()
 
     def check_reset(self) -> None:
         """Check if reset is possible and change the button state"""
-        if self.count.get() != Defaults.COUNT.value \
-                or self.check.instate([States.NOT_SELECTED.value]) \
-                or self.limit.get() != Defaults.LIMIT.value \
-                or self.button.instate([States.DISABLED.value]):
-            self.reset.state([States.NOT_DISABLED.value])
+        if self.count.get() != Default.COUNT.value \
+                or self.check.instate([State.NOT_SELECTED.value]) \
+                or self.limit.get() != Default.LIMIT.value \
+                or self.button.instate([State.DISABLED.value]):
+            self.reset.state([State.NOT_DISABLED.value])
         else:
-            self.reset.state([States.DISABLED.value])
+            self.reset.state([State.DISABLED.value])
 
     def toggle_logs(self) -> None:
         """Enable or disable logs console"""
-        if self.check.instate([States.SELECTED.value]):
+        if self.check.instate([State.SELECTED.value]):
             print("Enable logs")
         else:
             print("Disable logs")
@@ -129,14 +129,14 @@ class Counter:
 
     def log_if_enabled(self, text: str) -> None:
         """If Enable logs option is enabled, print text in console"""
-        if self.check.instate([States.SELECTED.value]):
+        if self.check.instate([State.SELECTED.value]):
             print(text)
 
     def increment(self) -> None:
         """Increment counter (+1) if possible, change state button if not"""
-        count = self.count.get()
+        count: int = self.count.get()
         if count >= self.limit.get():
-            self.button.state([States.DISABLED.value])
+            self.button.state([State.DISABLED.value])
             self.log_if_enabled("You have reached the limit")
         else:
             self.log_if_enabled("+1")
@@ -146,17 +146,17 @@ class Counter:
     def reset(self) -> None:
         """Reset all values to default values"""
         self.log_if_enabled("Reset values")
-        self.count.set(Defaults.COUNT.value)
-        self.check.state(Defaults.CHECK_STATE.value)
-        self.limit.set(Defaults.LIMIT.value)
-        self.button.state(Defaults.BUTTON_STATE.value)
-        self.reset.state([States.DISABLED.value])
+        self.count.set(Default.COUNT.value)
+        self.check.state(Default.CHECK_STATE.value)
+        self.limit.set(Default.LIMIT.value)
+        self.button.state(Default.BUTTON_STATE.value)
+        self.reset.state([State.DISABLED.value])
 
     def set_limit(self) -> None:
         """Set limit to requested number"""
-        limit = self.limit.get()
+        limit: int = self.limit.get()
         if self.count.get() < limit:
-            self.button.state([States.NOT_DISABLED.value])
+            self.button.state([State.NOT_DISABLED.value])
         self.log_if_enabled(f"You have put the limit to {limit}")
         self.check_reset()
 
